39e6ba82a41e083d114d7471f4088f5c31bf1f7e
Sarang Sanjay Bhimanwar <117634221+sarangbhimanwar2712@users.noreply.github.com>
2023-04-11 20:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
+  const numberOfBooksRead = () => {
+    let count = 0 ;
+    library.forEach(callMe(count)) ;
+    return count ;
+  };
+  function callMe(count){
+     if(readingStatus==true){
+        count++ ;
+     }
+     return count ;
+  };
 
-// Do not change the code below
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
 
-alert(numberOfBooksRead());
